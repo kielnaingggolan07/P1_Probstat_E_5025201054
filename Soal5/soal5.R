2d481fea3a202f8bede8f8683a5e3ae925ac11c1
Hesekiel Nainggolan <94334247+Kielgolan10@users.noreply.github.com>
2022-04-10 22:08:46 +0700

    Update soal5.R

--- Soal5/soal5.R
+++ Soal5/soal5.R
@@ -7,22 +7,22 @@ dexp(data.x, alpa)
 #soal5b
 # jika n =10        
 n <- 10
-set_data <- rexp(n, alpa = 3)
+set_data <- rexp(n, rate = 3)
 hist(set_data)
 
 # jika n = 100 
 n <- 100
-set_data <- rexp(n, alpa = 3)
+set_data <- rexp(n, rate = 3)
 hist(set_data)
 
 # jika n = 1,000
 n <- 1000
-set_data <- rexp(n, alpa = 3)
+set_data <- rexp(n, rate = 3)
 hist(set_data)
 
 # jika n = 10,000  
 n <- 10000
-set_data <- rexp(n, alpa = 3)
+set_data <- rexp(n, rate = 3)
 hist(set_data)
 
 #soal5c
